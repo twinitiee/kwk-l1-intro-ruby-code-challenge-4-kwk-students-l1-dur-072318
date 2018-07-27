6d7404242040c7c4a94d772072a31a962c4e3a0c
twinitiee <twinitiee@gmail.com>
2018-07-27 13:02:13 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,14 @@
 #write out your code here
 
 def least_coins(cents)
-
-#Code your answer here!
-
+coins = {}
+Q ==cents / 25
+R ==cents % 25
+D ==R / 10
+R ==R % 10 
+N ==R / 5
+R ==R % 5
+P ==R / 1
+coins[quarters, Q]
 end
+
